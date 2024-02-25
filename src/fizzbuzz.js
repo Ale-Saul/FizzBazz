c1a17b95f7a5999739c41cd385dc7f816a6d69ff
Alejandro <alejandro.mont.cesp@gmail.com>
2024-02-24 23:18:36 -0400

    se añadio la funcionalidad buzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,6 +2,9 @@ function fizzbuzz(n){
     if (n % 3 === 0){
         return "fizz";
     }
+    else if (n % 5 === 0){
+        return "buzz";
+    }
     else{
         return n + "";
     }
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -3,7 +3,13 @@ describe('fizzbuzz', () => {
     it('deberia entrar 1 y retornar 1', () => {
         expect(fizzbuzz(1)).toEqual("1");
     });
+    it('deberia entrar 2 y retornar 2', () => {
+        expect(fizzbuzz(2)).toEqual("2");
+    });
     it('deberia entrar 3 y retornar fizz', () => {
         expect(fizzbuzz(3)).toEqual("fizz");
+    });
+    it('deberia entrar 5 y retornar buzz', () => {
+        expect(fizzbuzz(5)).toEqual("buzz");
     });
 });
